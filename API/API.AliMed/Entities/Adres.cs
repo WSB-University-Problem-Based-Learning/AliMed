0557762efb74ce36c0f7984f75b97d673e2a9db0
DamL98 <damian.litewka@gmail.com>
2025-11-28 13:53:15 +0100

    basic entities for db migration #2

---
+++ API/API.AliMed/Entities/Adres.cs
@@ -0,0 +1,11 @@
+﻿namespace API.AliMed.Entities
+{
+    public class Adres
+    {
+        public string? Ulica { get; set; }
+        public string? NumerDomu { get; set; }
+        public string? KodPocztowy { get; set; }
+        public string? Miasto { get; set; }
+        public string Kraj { get; set; } = "Polska"; // default val
+    }
+}
---
+++ API/API.AliMed/Entities/Wizyta.cs
@@ -0,0 +1,25 @@
+﻿namespace API.AliMed.Entities
+{
+    public class Wizyta
+    {
+        public int WizytaId { get; set; }
+
+        public DateTime DataWizyty { get; set; }
+        public string? Diagnoza { get; set; }
+        public bool CzyOdbyta { get; set; }
+
+        // --- Relacje i klucze obce (FK) ---
+
+        // foreign key do Pacjenta
+        public int PacjentId { get; set; }
+        public Pacjent? Pacjent { get; set; }
+
+        // foreign key do Lekarza
+        public int LekarzId { get; set; }
+        public Lekarz? Lekarz { get; set; }
+
+        // foreign key do Placowki
+        public int PlacowkaId { get; set; }
+        public Placowka? Placowka { get; set; }
+    }
+}
