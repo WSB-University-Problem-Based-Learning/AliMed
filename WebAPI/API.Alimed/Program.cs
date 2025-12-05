71cbec88c45f67ed8f8610e64f1450f35287f20f
DamL98 <damian.litewka@gmail.com>
2025-12-05 10:11:31 +0100

    Updated Program.cs entrypoint for using Services / CORS / JWT

--- WebAPI/API.Alimed/Program.cs
+++ WebAPI/API.Alimed/Program.cs
@@ -1,4 +1,7 @@
 using API.Alimed.Data;
+using API.Alimed.Extensions;
+using API.Alimed.Interfaces;
+using API.Alimed.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +12,29 @@ var mysqlConn = builder.Configuration.GetConnectionString("MySqlConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(mysqlConn, ServerVersion.AutoDetect(mysqlConn)));
 
-builder.Services.AddControllers();
+// Dodanie konfiguracji JWT
+builder.Services.AddScoped<IJwtService, JwtService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddCustomServices(builder.Configuration);
+
+builder.Services.AddAuthorization(); // auth attributes for endpoints
+builder.Services.AddControllers(); // controller mapping
+builder.Services.AddHttpClient(); // http
 // builder.Services.AddOpenApi();
+
+
+// cors policy for dev testing 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173") // przepusc frontend z local dev
+                      .AllowAnyHeader()                     // przepusc all nag³ówki (w tym Content-Type i Authorization)
+                      .AllowAnyMethod();                    // dozwolone all requesty
+        });
+});
+
 
 var app = builder.Build();
 
@@ -20,10 +44,12 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 
+// app.UseHttpsRedirection();
+// CORS
+app.UseCors("AllowReactApp");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
